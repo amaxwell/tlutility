68d57bd9c456cb0b6e8dc528356a539af9eb9b12
adam.r.maxwell <amaxwell@mac.com>
2009-08-16 22:12:21 +0000

    Use a separate download delegate object and runloop mode, so the countdown timer is now functional for http URLs.

---
+++ TLMDownload.m
@@ -0,0 +1,104 @@
+//
+//  TLMDownload.m
+//  TeX Live Manager
+//
+//  Created by Adam Maxwell on 8/16/09.
+/*
+ This software is Copyright (c) 2009
+ Adam Maxwell. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ - Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ - Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in
+ the documentation and/or other materials provided with the
+ distribution.
+ 
+ - Neither the name of Adam Maxwell nor the names of any
+ contributors may be used to endorse or promote products derived
+ from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "TLMDownload.h"
+
+
+@implementation TLMDownload
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _data = [NSMutableData new];
+        _finished = NO;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [_connection cancel];
+    [_connection release];
+    [_data release];
+    [_error release];
+    [super dealloc];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [_data appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection;
+{
+    _finished = YES;
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    _finished = YES;
+    _error = [error retain];
+}
+
+- (void)downloadRequest:(NSURLRequest *)aRequest inMode:(NSString *)rlmode;
+{
+    NSParameterAssert(nil == _connection);
+    _connection = [[NSURLConnection alloc] initWithRequest:aRequest delegate:self startImmediately:NO];
+    [_connection scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:rlmode];
+    [_connection start];
+}
+
+- (void)cancel;
+{
+    [_connection cancel];
+}
+
+- (BOOL)failed:(NSError **)outError;
+{
+    if (outError)
+        *outError = _error;
+    return (nil != _error);
+}
+
+- (BOOL)isFinished;
+{
+    return _finished;
+}
+
+@end
