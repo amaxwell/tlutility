c9cd0a5b97c129f347db8a0b104731bde80eb542
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2009-08-31 16:45:00 +0000

    Allow compilation on 10.5 with informal protocols

--- trunk/TLMMainWindowController.h
+++ trunk/TLMMainWindowController.h
@@ -51,8 +51,11 @@
 @property (nonatomic, retain) TLMStatusWindow *statusWindow;
 @end
 
-
+#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_5
 @interface TLMMainWindowController : NSWindowController <TLMTabViewDelegate, NSWindowDelegate>
+#else
+@interface TLMMainWindowController : NSWindowController <TLMTabViewDelegate>
+#endif
 {
 @private
     NSProgressIndicator      *_progressIndicator;
