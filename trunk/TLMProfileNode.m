39337ba9cf5c787d4dda6cbf6e1b04576a86bd31
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2010-01-11 21:17:03 +0000

    update copyrights

--- trunk/TLMProfileNode.m
+++ trunk/TLMProfileNode.m
@@ -4,7 +4,7 @@
 //
 //  Created by Adam Maxwell on 09/12/09.
 /*
- This software is Copyright (c) 2009
+ This software is Copyright (c) 2009-2010
  Adam Maxwell. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
