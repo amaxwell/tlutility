0627b2b803bfb171cb749ede089734cbbe2843b2
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2010-03-14 20:50:13 +0000

    add example using DTProgress

---
+++ datatank_py/examples/count_progress.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os, sys
+from datatank_py.DTDataFile import DTDataFile
+from datatank_py.DTProgress import DTProgress
+from time import clock, sleep
+
+if __name__ == '__main__':
+    
+    #
+    # This program replaces a standard DataTank External Program module
+    # in C++.  Note that it must be executable (chmod 755 in Terminal).
+    #
+    
+    input_file = DTDataFile("Input.dtbin")
+    number = input_file["Number"]
+    input_file.close()
+    
+    # record start time and create a list for errors
+    start_time = clock()
+    errors = []
+    
+    values = []
+    progress = DTProgress()
+    
+    # sleep in a loop just to slow things down enough
+    for idx in xrange(0, int(number)):
+        values.append(idx)
+        progress.update_percentage(idx / float(number))
+        sleep(0.1)
+    
+    # create or truncate the output file    
+    with DTDataFile("Output.dtbin", truncate=True) as output_file:
+        # record computation time
+        output_file["ExecutionTime"] = clock() - start_time
+        
+        # DataTank seems to display stderr instead of the error list, so
+        # make sure to write to both.
+        if len(errors):
+            output_file["ExecutionErrors"] = errors
+            sys.stderr.write("%s\n" % errors)
+            
+        else:
+            # save the output variable; this will be visible to DataTank
+            output_file["Var"] = values
