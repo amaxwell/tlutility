6f100d5fd853275c9dc996ee081008d4c8f3cc6e
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2010-03-14 18:25:35 +0000

    add Python implementation of DTProgress

---
+++ datatank_py/DTProgress.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from math import floor
+
+class DTProgress(object):
+    """Drive progress indicator for DataTank.
+    
+    Call update_percentage periodically to get correct progress bar
+    timing during a long-running external program.  The implementation
+    creates a file called DTProgress in the current working directory.
+    
+    http://www.visualdatatools.com/phpBB2/viewtopic.php?t=158
+    
+    """
+            
+    def __init__(self):
+        super(DTProgress, self).__init__()
+        self._current_length = 0
+        
+    def update_percentage(self, percent):
+        """Updates the progress indicator if needed.
+        
+        Only updates the progress file on integral percentage points, so
+        can be called as frequently as needed.
+        
+        """
+        
+        new_length = int(floor(percent * 100 + 0.5))
+        if self._current_length < new_length:
+                        
+            # The only difference from DTSource is that we open and close the
+            # file here instead of keeping it open, so we can ensure that it's
+            # cleaned up properly.
+            mode = "w" if self._current_length == 0 else "a"
+            with open("DTProgress", mode) as pfile:
+                # write a single character to the file for each percentage point
+                while self._current_length < new_length:
+                    pfile.write("x")
+                    self._current_length += 1
+        
+                assert self._current_length == new_length
+                pfile.flush()
+
+if __name__ == '__main__':
+    
+    progress = DTProgress()
+    for idx in xrange(0, 300):
+        progress.update_percentage(idx / 300.)
+    
+    import os
+    assert os.path.getsize("DTProgress") == 100
