8f7366b6412c8bf1ce96942b643ec4ab3aace96b
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2010-04-13 23:53:36 +0000

    add docs and __all__

--- datatank_py/DTError.py
+++ datatank_py/DTError.py
@@ -3,15 +3,44 @@
 
 # This software is under a BSD license.  See LICENSE.txt for details.
 
+__all__ = ["DTErrorMessage", "DTSaveError"]
+
 import sys
 
 _errors = []
 
 def DTErrorMessage(fcn, msg):
+    """Accumulate a message and echo to standard error.
+    
+    Arguments:
+    fcn -- typically a function or module name
+    msg -- an error or warning message
+    
+    Returns:
+    Nothing
+    
+    Typically you call this each time an error or warning
+    should be presented, then call DTSaveError before exiting.
+    
+    """
+    
     err_msg = "%s: %s" % (fcn, msg)
     _errors.append(err_msg)
     sys.stderr.write(err_msg + "\n")
 
-def DTSaveError(datafile, name):
+def DTSaveError(datafile, name="ExecutionErrors"):
+    """Save accumulated messages to a file.
+    
+    Arguments:
+    datafile -- an open DTDataFile instance
+    name -- defaults to "ExecutionErrors" for DataTank
+    
+    Returns:
+    Nothing
+    
+    This will be displayed in DataTank's Messages panel.
+    
+    """
+    
     if len(_errors):
         datafile[name] = _errors
