9c8565c7ffd1b18b1b90ba4b59573bd9849f3664
adam.r.maxwell <amaxwell@mac.com>
2010-11-18 15:20:38 +0000

    add empty windowcontroller and xib for mirrors

---
+++ TLMMirrorController.h
@@ -0,0 +1,16 @@
+//
+//  TLMMirrorController.h
+//  TeX Live Manager
+//
+//  Created by Adam R. Maxwell on 11/18/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TLMMirrorController : NSWindowController {
+
+}
+
+@end
---
+++ TLMMirrorController.m
@@ -0,0 +1,14 @@
+//
+//  TLMMirrorController.m
+//  TeX Live Manager
+//
+//  Created by Adam R. Maxwell on 11/18/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "TLMMirrorController.h"
+
+
+@implementation TLMMirrorController
+
+@end
