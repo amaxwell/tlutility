d29522dd2166c54a4f5b6659d7b103dd04ed10e3
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2011-01-06 04:41:22 +0000

    update copyright

--- DVI-qlgenerator/GeneratePreviewForURL.c
+++ DVI-qlgenerator/GeneratePreviewForURL.c
@@ -1,5 +1,5 @@
 /*
- This software is Copyright (c) 2009-2010
+ This software is Copyright (c) 2009-2011
  Adam Maxwell. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
