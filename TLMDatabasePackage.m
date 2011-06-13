3613444f4c48fb76b159e34d880c8a8d091034d4
adam.r.maxwell <amaxwell@mac.com>
2011-06-13 14:10:40 +0000

    do not modify files arrays in database

--- TLMDatabasePackage.m
+++ TLMDatabasePackage.m
@@ -52,46 +52,6 @@
     self = [super init];
     if (self) {
         _dictionary = [dict mutableCopy];
-        NSString *installPath = [[[TLMPreferenceController sharedPreferenceController] installDirectory] path];
-        NSFileManager *fm = [NSFileManager new];
-        NSString *keys[] = { @"runFiles", @"sourceFiles", @"docFiles" };
-        for (NSUInteger i = 0; i < sizeof(keys) / sizeof(NSString *); i++) {
-            NSMutableArray *files = [[dict objectForKey:keys[i]] mutableCopy];
-            
-            // skip this round since we can't add a nil array to the dictionary
-            if (nil == files)
-                continue;
-            
-            // iterate backwards to modify the array in-place
-            NSUInteger fidx = [files count];
-            while (fidx--) {
-                NSString *path = [files objectAtIndex:fidx];
-                
-                // have to munge paths with RELOC from the remote tlpdb
-                if ([path hasPrefix:@"RELOC"])
-                    path = [path stringByReplacingCharactersInRange:NSMakeRange(0, 5) withString:@"texmf-dist"];
-                path = [installPath stringByAppendingPathComponent:path];
-                
-                if ([fm fileExistsAtPath:path]) {
-                    NSURL *furl = [[NSURL alloc] initFileURLWithPath:path];
-                    if (furl) {
-                        [files replaceObjectAtIndex:fidx withObject:furl];
-                        [furl release];
-                    }
-                    else {
-                        // invalid URL
-                        [files removeObjectAtIndex:fidx];
-                    }
-                }
-                else {
-                    // nonexistent file
-                    [files removeObjectAtIndex:fidx];
-                }                    
-            }
-            [_dictionary setObject:files forKey:keys[i]];
-            [files release];
-        }
-        [fm release];
     }
     return self;
 }
@@ -118,6 +78,43 @@
     return [[[NSAttributedString alloc] initWithString:desc attributes:nil] autorelease];
 }
 
+- (NSArray *)_absoluteURLsFromDatabaseFiles:(NSArray *)dbfiles
+{
+    NSString *installPath = [[[TLMPreferenceController sharedPreferenceController] installDirectory] path];
+    NSFileManager *fm = [NSFileManager new];
+    
+    NSMutableArray *files = [dbfiles mutableCopy];
+    
+    // iterate backwards to modify the array in-place
+    NSUInteger fidx = [files count];
+    while (fidx--) {
+        NSString *path = [files objectAtIndex:fidx];
+        
+        // have to munge paths with RELOC from the remote tlpdb
+        if ([path hasPrefix:@"RELOC"])
+            path = [path stringByReplacingCharactersInRange:NSMakeRange(0, 5) withString:@"texmf-dist"];
+        path = [installPath stringByAppendingPathComponent:path];
+        
+        if ([fm fileExistsAtPath:path]) {
+            NSURL *furl = [[NSURL alloc] initFileURLWithPath:path];
+            if (furl) {
+                [files replaceObjectAtIndex:fidx withObject:furl];
+                [furl release];
+            }
+            else {
+                // invalid URL
+                [files removeObjectAtIndex:fidx];
+            }
+        }
+        else {
+            // nonexistent file
+            [files removeObjectAtIndex:fidx];
+        }                    
+    }
+    [fm release];
+    return [files autorelease];
+}
+
 TLM_METHOD(NSString*, name)
 TLM_METHOD(NSString*, category)
 TLM_METHOD(NSString*, shortDescription)
@@ -126,8 +123,19 @@ TLM_METHOD(NSNumber*, relocated)
 TLM_METHOD(NSNumber*, revision)
 
 // override to return arrays of URL objects (only for files that exist on-disk)
-TLM_METHOD(NSArray*, runFiles)
-TLM_METHOD(NSArray*, sourceFiles)
-TLM_METHOD(NSArray*, docFiles)
+- (NSArray *)runFiles
+{
+    return [self _absoluteURLsFromDatabaseFiles:[_dictionary objectForKey:@"runFiles"]];
+}
+
+- (NSArray *)sourceFiles
+{
+    return [self _absoluteURLsFromDatabaseFiles:[_dictionary objectForKey:@"sourceFiles"]];    
+}
+
+- (NSArray *)docFiles
+{
+    return [self _absoluteURLsFromDatabaseFiles:[_dictionary objectForKey:@"docFiles"]];    
+}
 
 @end
