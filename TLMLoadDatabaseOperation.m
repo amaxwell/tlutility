d1809e649cdedd99e08c6aa2d3df9bb26601425d
adam.r.maxwell <amaxwell@mac.com>
2011-06-16 14:14:13 +0000

    Refactor to get rid of hideous code duplication

--- TLMLoadDatabaseOperation.m
+++ TLMLoadDatabaseOperation.m
@@ -90,86 +90,19 @@
     [[aNote object] readInBackgroundAndNotify];    
 }
 
-#define DEBUG_TLPDB 0
-
-- (void)main
+// nil URL indicates the local db
+- (BOOL)_dumpDatabaseAtURL:(NSURL *)aURL asPropertyList:(NSString *)absolutePath
 {
-    NSAutoreleasePool *pool = [NSAutoreleasePool new];
-
     NSString *tlmgrPath = [[TLMEnvironment currentEnvironment] tlmgrAbsolutePath];
-    BDSKTask *dumpTask = nil, *parseTask = nil;
-    NSArray *arguments = nil;
-    
-#if DEBUG_TLPDB
-    NSString *temporaryPath = [NSString stringWithFormat:@"/tmp/TLMLoadDatabaseOperation_%p.tlpdb", self];
-#else
-    CFUUIDRef uuid = CFUUIDCreate(NULL);
-    NSString *temporaryPath = [NSTemporaryDirectory() stringByAppendingPathComponent:[(id)CFUUIDCreateString(NULL, uuid) autorelease]];
-    if (uuid) CFRelease(uuid);
-#endif
-    
-    if (NO == _offline) {
-        dumpTask = [[BDSKTask new] autorelease];
-        [dumpTask setLaunchPath:tlmgrPath];
-        arguments = [NSArray arrayWithObjects:@"--repository", [[self updateURL] absoluteString], @"dump-tlpdb", @"--remote", nil];
-        [dumpTask setArguments:arguments];
-        [dumpTask setStandardOutput:[NSPipe pipe]];
-        [dumpTask setStandardError:[NSPipe pipe]];
-        [[NSNotificationCenter defaultCenter] addObserver:self 
-                                                 selector:@selector(_dumpDataAvailable:) 
-                                                     name:NSFileHandleDataAvailableNotification 
-                                                   object:[[dumpTask standardError] fileHandleForReading]];
-        [[[dumpTask standardError] fileHandleForReading] readInBackgroundAndNotify];
-        [dumpTask launch];
-        
-        parseTask = [[BDSKTask new] autorelease];
-        [parseTask setLaunchPath:[[NSBundle mainBundle] pathForAuxiliaryExecutable:@"parse_tlpdb.py"]];
-        [parseTask setArguments:[NSArray arrayWithObjects:@"-o", temporaryPath, @"-f", @"plist", nil]];
-        [parseTask setStandardInput:[[dumpTask standardOutput] fileHandleForReading]];
-        [parseTask setStandardError:[NSPipe pipe]];
-        [[NSNotificationCenter defaultCenter] addObserver:self 
-                                                 selector:@selector(_parseDataAvailable:) 
-                                                     name:NSFileHandleDataAvailableNotification 
-                                                   object:[[parseTask standardError] fileHandleForReading]];
-        [[[parseTask standardError] fileHandleForReading] readInBackgroundAndNotify];
-        [parseTask launch];
-        
-        [parseTask waitUntilExit];
 
-        if ([parseTask terminationStatus] == EXIT_SUCCESS) {
-            NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:temporaryPath];
-            NSString *mirror = [dict objectForKey:@"mirror"];
-            if (nil == mirror || [NSURL URLWithString:mirror] == nil)
-                TLMLog(__func__, @"Unable to read mirror from tlpdb property list with keys %@", [dict allKeys]);
-            else
-                [self setUpdateURL:[NSURL URLWithString:mirror]];
-            [[TLMDatabase databaseForURL:[self updateURL]] reloadDatabaseFromPath:temporaryPath];
-        }
-        else {
-            TLMLog(__func__, @"Dumping tlpdb from mirror %@ failed", [self updateURL]);
-        } 
-        
-        [[NSNotificationCenter defaultCenter] removeObserver:self 
-                                                        name:NSFileHandleDataAvailableNotification 
-                                                      object:[[parseTask standardError] fileHandleForReading]];
-        [[NSNotificationCenter defaultCenter] removeObserver:self 
-                                                        name:NSFileHandleDataAvailableNotification 
-                                                      object:[[dumpTask standardError] fileHandleForReading]];       
-        [_parseError appendData:[[[parseTask standardError] fileHandleForReading] availableData]];
-        [_dumpError appendData:[[[dumpTask standardError] fileHandleForReading] availableData]];
-        if ([_parseError length]) {
-            TLMLog(__func__, @"Parse error: %@",[[[NSString alloc] initWithData:_parseError encoding:NSUTF8StringEncoding] autorelease]);
-            [_parseError setData:[NSData data]];
-        }
-        if ([_dumpError length]) {
-            TLMLog(__func__, @"Dump error: %@",[[[NSString alloc] initWithData:_dumpError encoding:NSUTF8StringEncoding] autorelease]);
-            [_dumpError setData:[NSData data]];
-        }
-    }
-        
-    dumpTask = [[BDSKTask new] autorelease];
+    BDSKTask *dumpTask = [[BDSKTask new] autorelease];
     [dumpTask setLaunchPath:tlmgrPath];
-    arguments = [NSArray arrayWithObjects:@"dump-tlpdb", @"--local", nil];
+    NSArray *arguments = nil;
+    if (aURL)
+        arguments = [NSArray arrayWithObjects:@"--repository", [[self updateURL] absoluteString], @"dump-tlpdb", @"--remote", nil];
+    else
+        arguments = [NSArray arrayWithObjects:@"dump-tlpdb", @"--local", nil];
+    
     [dumpTask setArguments:arguments];
     [dumpTask setStandardOutput:[NSPipe pipe]];
     [dumpTask setStandardError:[NSPipe pipe]];
@@ -180,9 +113,9 @@
     [[[dumpTask standardError] fileHandleForReading] readInBackgroundAndNotify];
     [dumpTask launch];
     
-    parseTask = [[BDSKTask new] autorelease];
+    BDSKTask *parseTask = [[BDSKTask new] autorelease];
     [parseTask setLaunchPath:[[NSBundle mainBundle] pathForAuxiliaryExecutable:@"parse_tlpdb.py"]];
-    [parseTask setArguments:[NSArray arrayWithObjects:@"-o", temporaryPath, @"-f", @"plist", nil]];
+    [parseTask setArguments:[NSArray arrayWithObjects:@"-o", absolutePath, @"-f", @"plist", nil]];
     [parseTask setStandardInput:[[dumpTask standardOutput] fileHandleForReading]];
     [parseTask setStandardError:[NSPipe pipe]];
     [[NSNotificationCenter defaultCenter] addObserver:self 
@@ -194,21 +127,18 @@
     
     [parseTask waitUntilExit];
     
-    if ([parseTask terminationStatus] == EXIT_SUCCESS) {
-        [[TLMDatabase localDatabase] reloadDatabaseFromPath:temporaryPath];
-    }
-    else {
-        TLMLog(__func__, @"Dumping local tlpdb failed");
-    }
-    
+    // no more runloop running, so won't be picking these up
     [[NSNotificationCenter defaultCenter] removeObserver:self 
                                                     name:NSFileHandleDataAvailableNotification 
                                                   object:[[parseTask standardError] fileHandleForReading]];
     [[NSNotificationCenter defaultCenter] removeObserver:self 
                                                     name:NSFileHandleDataAvailableNotification 
-                                                  object:[[dumpTask standardError] fileHandleForReading]];  
+                                                  object:[[dumpTask standardError] fileHandleForReading]];       
+    
+    // get any residual data
     [_parseError appendData:[[[parseTask standardError] fileHandleForReading] availableData]];
     [_dumpError appendData:[[[dumpTask standardError] fileHandleForReading] availableData]];
+    
     if ([_parseError length]) {
         TLMLog(__func__, @"Parse error: %@",[[[NSString alloc] initWithData:_parseError encoding:NSUTF8StringEncoding] autorelease]);
         [_parseError setData:[NSData data]];
@@ -216,6 +146,48 @@
     if ([_dumpError length]) {
         TLMLog(__func__, @"Dump error: %@",[[[NSString alloc] initWithData:_dumpError encoding:NSUTF8StringEncoding] autorelease]);
         [_dumpError setData:[NSData data]];
+    }
+    
+    return ([parseTask terminationStatus] == EXIT_SUCCESS);
+}
+
+#define DEBUG_TLPDB 0
+
+- (void)main
+{
+    NSAutoreleasePool *pool = [NSAutoreleasePool new];
+    
+#if DEBUG_TLPDB
+    NSString *temporaryPath = [NSString stringWithFormat:@"/tmp/TLMLoadDatabaseOperation_%p.tlpdb", self];
+#else
+    CFUUIDRef uuid = CFUUIDCreate(NULL);
+    NSString *temporaryPath = [NSTemporaryDirectory() stringByAppendingPathComponent:[(id)CFUUIDCreateString(NULL, uuid) autorelease]];
+    if (uuid) CFRelease(uuid);
+#endif
+        
+    // can only dump the remote db if we're not in offline mode
+    if (NO == _offline) {
+                
+        if ([self _dumpDatabaseAtURL:[self updateURL] asPropertyList:temporaryPath]) {
+            NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:temporaryPath];
+            NSString *mirror = [dict objectForKey:@"mirror"];
+            if (nil == mirror || [NSURL URLWithString:mirror] == nil)
+                TLMLog(__func__, @"Unable to read mirror from tlpdb property list with keys %@", [dict allKeys]);
+            else
+                [self setUpdateURL:[NSURL URLWithString:mirror]];
+            [[TLMDatabase databaseForURL:[self updateURL]] reloadDatabaseFromPath:temporaryPath];
+        }
+        else {
+            TLMLog(__func__, @"Dumping tlpdb from mirror %@ failed", [self updateURL]);
+        } 
+    }
+    
+    // always dump the local db
+    if ([self _dumpDatabaseAtURL:nil asPropertyList:temporaryPath]) {
+        [[TLMDatabase localDatabase] reloadDatabaseFromPath:temporaryPath];
+    }
+    else {
+        TLMLog(__func__, @"Dumping local tlpdb failed");
     }
     
 #if !(DEBUG_TLPDB)
