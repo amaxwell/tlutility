c3676d11683455385a9065275a1bfd3c744058ac
adam.r.maxwell <amaxwell@mac.com>
2011-07-17 19:27:09 +0000

    Move log tableview to a separate nib and out of the main window.

--- TLMLogWindowController.m
+++ TLMLogWindowController.m
@@ -1,52 +1,23 @@
 //
-//  TLMLogDataSource.m
+//  TLMLogWindowController.m
 //  TeX Live Manager
 //
-//  Created by Adam Maxwell on 12/12/08.
-/*
- This software is Copyright (c) 2008-2011
- Adam Maxwell. All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
- 
- - Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- 
- - Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in
- the documentation and/or other materials provided with the
- distribution.
- 
- - Neither the name of Adam Maxwell nor the names of any
- contributors may be used to endorse or promote products derived
- from this software without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-#import "TLMLogDataSource.h"
+//  Created by Adam R. Maxwell on 07/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TLMLogWindowController.h"
 #import "TLMLogMessage.h"
 #import "TLMLogServer.h"
 #import "TLMTableView.h"
 
-@implementation TLMLogDataSource
+@implementation TLMLogWindowController
 
 @synthesize _tableView;
 
 - (id)init
 {
-    self = [super init];
+    self = [self initWithWindowNibName:[self windowNibName]];
     if (self) {
         [[NSNotificationCenter defaultCenter] addObserver:self 
                                                  selector:@selector(_handleLogServerUpdateNotification:) 
@@ -61,7 +32,7 @@
         // pointer equality dictionary, non-copying (since TLMLogMessage is technically mutable)
         _rowHeights = CFDictionaryCreateMutable(CFAllocatorGetDefault(), 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
     }
-    return self;
+    return self;    
 }
 
 - (void)dealloc
@@ -77,6 +48,8 @@
     
     [super dealloc];
 }
+
+- (NSString *)windowNibName { return @"LogWindow"; }
 
 - (void)_update
 {
@@ -174,7 +147,7 @@
     if (obj) height = CFDictionaryGetValue(_rowHeights, obj);
     
     if (NULL == height) {
-            
+        
         // pass an "infinitely" tall rect for cell bounds, and let the cell figure out the string height it needs
         NSRect cellBounds = NSZeroRect;
         cellBounds.size = NSMakeSize([tc width], CGFLOAT_MAX);
