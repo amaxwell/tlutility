018dddff196f5c51fc577f50d88cfba46a72ba85
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2011-10-22 18:11:25 +0000

    fix path to xcodebuild

--- trunk/build_beta.py
+++ trunk/build_beta.py
@@ -102,7 +102,7 @@ def rewrite_version():
 def clean_and_build():
     
     # clean and rebuild the Xcode project
-    buildCmd = ["/usr/bin/xcodebuild", "-configuration", "Release", "-target", "TeX Live Utility", "clean", "build"]
+    buildCmd = ["/Xcode3/usr/bin/xcodebuild", "-configuration", "Release", "-target", "TeX Live Utility", "clean", "build"]
     nullDevice = open("/dev/null", "r")
     x = Popen(buildCmd, cwd=SOURCE_DIR, stdout=nullDevice, stderr=nullDevice)
     rc = x.wait()
