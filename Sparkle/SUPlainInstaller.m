976f2a07da6e5ea25b57d2d396ec7a8cfa3064d7
adam.r.maxwell <amaxwell@mac.com>
2011-10-22 17:41:30 +0000

    add import to silence warning

--- Sparkle/SUPlainInstaller.m
+++ Sparkle/SUPlainInstaller.m
@@ -8,6 +8,7 @@
 
 #import "SUPlainInstaller.h"
 #import "SUPlainInstallerInternals.h"
+#import "SUHost.h"
 
 NSString *SUInstallerPathKey = @"SUInstallerPath";
 NSString *SUInstallerTargetPathKey = @"SUInstallerTargetPath";
