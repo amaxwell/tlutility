b21acb99087283669337f84bafa4c24630b74059
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2012-08-14 22:37:01 +0000

    Add script to uninstall the /Library agent files

---
+++ trunk/agent/uninstall_local_agent.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#
+# Uninstall the launchd agent from the local domain (/Library).
+# Requires authorization to run, so rights must be acquired
+# before calling the script.
+#
+
+PLIST_PATH="/Library/LaunchAgents/com.googlecode.mactlmgr.update_check.plist"
+SCRIPT_PATH="/Library/Application Support/TeX Live Utility/update_check.py"
+
+if [ -f "$PLIST_PATH" ]; then
+    if /bin/launchctl unload -w -S Aqua "$PLIST_PATH" ; then
+        echo "unloaded $PLIST_PATH" >&2
+    else
+        echo "failed to unload $PLIST_PATH" >&2
+    fi
+    
+    if rm "$PLIST_PATH" ; then
+        echo "removed $PLIST_PATH" >&2
+    else
+        echo "failed to remove $PLIST_PATH" >&2
+    fi
+fi
+
+if [ -f "$SCRIPT_PATH" ]; then
+    if rm "$SCRIPT_PATH" ; then
+        echo "removed $SCRIPT_PATH" >&2
+    else
+        echo "failed to remove $SCRIPT_PATH" >&2
+    fi
+fi
+
