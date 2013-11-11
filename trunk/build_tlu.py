8f9500f351fe3b2aa426abd89e10fcc8724580da
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2013-11-11 15:14:58 +0000

    update copyrights

--- trunk/build_tlu.py
+++ trunk/build_tlu.py
@@ -18,7 +18,7 @@
 #
 # Created by Adam Maxwell on 12/28/08.
 #
-# This software is Copyright (c) 2008-2010
+# This software is Copyright (c) 2008-2013
 # Adam Maxwell. All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
