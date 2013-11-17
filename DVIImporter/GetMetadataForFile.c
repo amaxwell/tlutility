99407e476c883bfb6daeefe9dd22be511e99fe6e
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2013-11-17 19:18:59 +0000

    update copyrights

--- DVIImporter/GetMetadataForFile.c
+++ DVIImporter/GetMetadataForFile.c
@@ -1,5 +1,5 @@
 /*
- This software is Copyright (c) 2005-2012
+ This software is Copyright (c) 2005-2013
  Adam Maxwell. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
