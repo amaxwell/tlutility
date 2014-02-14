d46eab5d9e2d86671459b829bc960b5e7d69a247
adam.r.maxwell <adam.r.maxwell@48dbfc66-c745-11dd-8829-37334faa441c>
2014-02-14 18:29:30 +0000

    call realpath if we have a symlink; see bug #85

--- trunk/python_version.py
+++ trunk/python_version.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
 sys.stdout.write("Using Python interpreter at '%s'\n" % (sys.executable))
-sys.stdout.write("Python version: %s" % (sys.version))
+sys.stdout.write("Python version: %s\n" % (sys.version))
+if os.path.islink(sys.executable):
+    sys.stderr.write("\n*** WARNING *** '%s' is a symlink to '%s'. Replacing the system python is not supported" % (sys.executable, os.path.realpath(sys.executable)))
 major, minor = sys.version_info[0:2]
 if major > 2:
     sys.stderr.write("\n*** WARNING *** Python scripts used by TeX Live Utility may not be compatible with Python %s.%s" % (major, minor))
